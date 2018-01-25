e099829ca8cd97a11159f770a6e6b9f31b902afb
TianyouZhang721 <zhangty_0721@163.com>
2018-01-25 17:10:56 +0800

    新建组件

--- src/app/components/designer/designer.module.ts
+++ src/app/components/designer/designer.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShareModule } from 'app/share/share.module';
 import { DesignerListComponent } from './designer-list/designer-list.component';
+import { DesignersComponent } from './designers/designers.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ShareModule
   ],
-  declarations: [DesignerListComponent]
+  declarations: [DesignerListComponent, DesignersComponent]
 })
 export class DesignerModule { }
--- src/app/components/shop-home/shop-home.module.ts
+++ src/app/components/shop-home/shop-home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShareModule } from 'app/share/share.module';
 import { ShopHomeComponent } from './shop-home/shop-home.component';
+import { ShopHomeListComponent } from './shop-home-list/shop-home-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ShareModule
   ],
-  declarations: [ShopHomeComponent]
+  declarations: [ShopHomeComponent, ShopHomeListComponent]
 })
 export class ShopHomeModule { }
