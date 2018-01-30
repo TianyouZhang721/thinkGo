50b99d2a81af96000e62e234696d926feb942063
TianyouZhang721 <zhangty_0721@163.com>
2018-01-30 18:03:06 +0800

    产品详情页头部样式下拉改变

--- src/app/components/product-intro/product-intro.component.ts
+++ src/app/components/product-intro/product-intro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute} from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import * as $ from 'jquery';
 declare let window: any;
 
@@ -8,9 +9,26 @@ declare let window: any;
   selector: 'app-product-intro',
   templateUrl: './product-intro.component.html',
   styleUrls: ['./product-intro.component.css'],
+  animations: [
+    trigger('heroState', [
+      state('inactive', style({
+      })),
+      state('active', style({
+        position: 'fixed',
+        height: '1rem',
+        top: 0,
+        left: 0,
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+      })),
+      transition('inactive => active', animate('500ms ease-in')),
+      transition('active => inactive', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class ProductIntroComponent implements OnInit {
   id:number;
+  scrollY: number = 0;
+  top: number = 0;
 
   constructor(
     private location: Location,
@@ -25,7 +43,18 @@ export class ProductIntroComponent implements OnInit {
         console.log(this.id);
       }
     )
+    this.onscroll();
 
+  }
+  onscroll() {
+    let oTop = $('.top-header')[0].offsetHeight;
+    this.top = oTop;
+    // console.log(oTop)
+    window.onscroll = () => {
+      // 获取页面滚动的距离
+      let t = document.documentElement.scrollTop || document.body.scrollTop;
+      this.scrollY = t;
+    }
   }
   goback() {
       this.location.back();
