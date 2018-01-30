576bb901df6de19a6f3e0d7dbee6ae33692c7d51
zhangtianyou <zhangty_0721@163.com>
2018-01-30 21:56:47 +0800

    修改详情页，加轮播效果

--- src/app/components/product-intro/product-intro.component.ts
+++ src/app/components/product-intro/product-intro.component.ts
@@ -29,6 +29,43 @@ export class ProductIntroComponent implements OnInit {
   id:number;
   scrollY: number = 0;
   top: number = 0;
+  proList : any = 
+    [
+      [
+        { 
+          pid:1,
+          src:"assets/img/guang/guang01.jpg,assets/img/guang/guang02.jpg,assets/img/guang/guang03.jpg",
+          title:'多功能口袋拼接',
+          price:'￥298.00'
+        }
+      ],
+      [
+        { 
+          pid:2,
+          src:"assets/img/guang/guang01.jpg,assets/img/guang/guang02.jpg,assets/img/guang/guang03.jpg",
+          title:'大牌气质斗篷外套',
+          price:'￥651.00'
+        }
+      ],
+      [
+        { 
+          pid:3,
+          src:"assets/img/guang/guang01.jpg,assets/img/guang/guang02.jpg,assets/img/guang/guang03.jpg",
+          title:'羊绒毛衣',
+          price:'￥333.00'
+        }
+      ],
+      [
+        { 
+          pid:4,
+          src:"assets/img/guang/guang01.jpg,assets/img/guang/guang02.jpg,assets/img/guang/guang03.jpg",
+          title:'喇叭裤',
+          price:'￥295.00'
+        }
+      ]
+    ];
+    srcArr:any;
+   
 
   constructor(
     private location: Location,
@@ -44,7 +81,11 @@ export class ProductIntroComponent implements OnInit {
       }
     )
     this.onscroll();
-
+    this.getArr();
+  }
+  getArr() {
+    this.srcArr = this.proList[this.id][0].src.split(',');
+    console.log(this.srcArr);
   }
   onscroll() {
     let oTop = $('.top-header')[0].offsetHeight;
@@ -57,24 +98,37 @@ export class ProductIntroComponent implements OnInit {
       if(this.scrollY>this.top){
         console.log(1);
         $('.productintro_return').css({
-          'background': 'rgba(0,0,0,0)'
+          'background': 'rgba(0,0,0,0)',
         });
         $('.productintro_right_1').css({
           'background': 'rgba(0,0,0,0)'
         });
         $('.productintro_right_2').css({
           'background': 'rgba(0,0,0,0)'
+        });
+        $('.productintro_return>a').css({
+          'color': '#000'
+        });
+        $('.productintro_right_1>a').css({
+          'color': '#000'
+        });
+        $('.productintro_right_2>a').css({
+          'color': '#000'
         });
       }else{
         $('.productintro_return').css({
-          'background': 'rgba(0,0,0,0.5)'
+          'background': 'rgba(0,0,0,0.5)',
+          'color':'#fff'
         });
         $('.productintro_right_1').css({
-          'background': 'rgba(0,0,0,0.5)'
+          'background': 'rgba(0,0,0,0.5)',
+          'color':'#fff'
         });
         $('.productintro_right_2').css({
-          'background': 'rgba(0,0,0,0.5)'
+          'background': 'rgba(0,0,0,0.5)',
+          'color':'#fff'
         });
+        
       }
     }
   }
